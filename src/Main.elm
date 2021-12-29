9ca5fec63d373fb63fd3fff6aaba5a9cbe8b88fa
Georgios Karametas <geometas@hotmail.com>
2021-12-29 11:32:54 +0000

    pipe, custom fn and modulo

--- src/Main.elm
+++ src/Main.elm
@@ -3,13 +3,23 @@ module Main exposing (main)
 import Html exposing (text)
 
 
+stringFromBool : Bool -> String
+stringFromBool value =
+    if value then
+        "True"
+
+    else
+        "False"
+
+
 add a b =
     a + b
 
 
 result =
-    add 1 2 |> add 3
+    -- add 1 2 |> add 3
+    add 2 2 |> (\a -> modBy 2 a == 0)
 
 
 main =
-    text (String.fromInt result)
+    text (stringFromBool result)
